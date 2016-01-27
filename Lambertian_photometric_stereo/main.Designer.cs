17a65ced29223c39e66261c5b9f3537c758835cf
c13proto <c13proto@aqua.ocn.ne.jp>
2016-01-27 20:41:31 +0900

    ベクトル計算だけ書いた

---
+++ Lambertian_photometric_stereo/main.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace Lambertian_photometric_stereo
+{
+    partial class main
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxIpl1 = new OpenCvSharp.UserInterface.PictureBoxIpl();
+            this.button_開く = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton_3 = new System.Windows.Forms.RadioButton();
+            this.radioButton_2 = new System.Windows.Forms.RadioButton();
+            this.radioButton_1 = new System.Windows.Forms.RadioButton();
+            this.radioButton_0 = new System.Windows.Forms.RadioButton();
+            this.textBox_camera = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_0b = new System.Windows.Forms.TextBox();
+            this.textBox_1b = new System.Windows.Forms.TextBox();
+            this.textBox_2b = new System.Windows.Forms.TextBox();
+            this.textBox_3b = new System.Windows.Forms.TextBox();
+            this.textBox_3a = new System.Windows.Forms.TextBox();
+            this.textBox_2a = new System.Windows.Forms.TextBox();
+            this.textBox_1a = new System.Windows.Forms.TextBox();
+            this.textBox_0a = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBoxIpl1
+            // 
+            this.pictureBoxIpl1.Location = new System.Drawing.Point(104, 3);
+            this.pictureBoxIpl1.Name = "pictureBoxIpl1";
+            this.pictureBoxIpl1.Size = new System.Drawing.Size(640, 480);
+            this.pictureBoxIpl1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxIpl1.TabIndex = 0;
+            this.pictureBoxIpl1.TabStop = false;
+            // 
+            // button_開く
+            // 
+            this.button_開く.Location = new System.Drawing.Point(3, 10);
+            this.button_開く.Name = "button_開く";
+            this.button_開く.Size = new System.Drawing.Size(75, 23);
+            this.button_開く.TabIndex = 1;
+            this.button_開く.Text = "開く";
+            this.button_開く.UseVisualStyleBackColor = true;
+            this.button_開く.Click += new System.EventHandler(this.Click_開く);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton_3);
+            this.panel1.Controls.Add(this.radioButton_2);
+            this.panel1.Controls.Add(this.radioButton_1);
+            this.panel1.Controls.Add(this.radioButton_0);
+            this.panel1.Location = new System.Drawing.Point(3, 39);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(95, 100);
+            this.panel1.TabIndex = 2;
+            // 
+            // radioButton_3
+            // 
+            this.radioButton_3.AutoSize = true;
+            this.radioButton_3.Location = new System.Drawing.Point(4, 73);
+            this.radioButton_3.Name = "radioButton_3";
+            this.radioButton_3.Size = new System.Drawing.Size(61, 16);
+            this.radioButton_3.TabIndex = 3;
+            this.radioButton_3.Text = "左下[3]";
+            this.radioButton_3.UseVisualStyleBackColor = true;
+            this.radioButton_3.Click += new System.EventHandler(this.Click_radio3);
+            // 
+            // radioButton_2
+            // 
+            this.radioButton_2.AutoSize = true;
+            this.radioButton_2.Location = new System.Drawing.Point(4, 49);
+            this.radioButton_2.Name = "radioButton_2";
+            this.radioButton_2.Size = new System.Drawing.Size(61, 16);
+            this.radioButton_2.TabIndex = 2;
+            this.radioButton_2.Text = "左上[2]";
+            this.radioButton_2.UseVisualStyleBackColor = true;
+            this.radioButton_2.Click += new System.EventHandler(this.Click_radio2);
+            // 
+            // radioButton_1
+            // 
+            this.radioButton_1.AutoSize = true;
+            this.radioButton_1.Location = new System.Drawing.Point(4, 27);
+            this.radioButton_1.Name = "radioButton_1";
+            this.radioButton_1.Size = new System.Drawing.Size(61, 16);
+            this.radioButton_1.TabIndex = 1;
+            this.radioButton_1.Text = "右下[1]";
+            this.radioButton_1.UseVisualStyleBackColor = true;
+            this.radioButton_1.Click += new System.EventHandler(this.Click_radio1);
+            // 
+            // radioButton_0
+            // 
+            this.radioButton_0.AutoSize = true;
+            this.radioButton_0.Location = new System.Drawing.Point(4, 4);
+            this.radioButton_0.Name = "radioButton_0";
+            this.radioButton_0.Size = new System.Drawing.Size(61, 16);
+            this.radioButton_0.TabIndex = 0;
+            this.radioButton_0.Text = "右上[0]";
+            this.radioButton_0.UseVisualStyleBackColor = true;
+            this.radioButton_0.Click += new System.EventHandler(this.Click_radio0);
+            // 
+            // textBox_camera
+            // 
+            this.textBox_camera.Location = new System.Drawing.Point(46, 157);
+            this.textBox_camera.Name = "textBox_camera";
+            this.textBox_camera.Size = new System.Drawing.Size(44, 19);
+            this.textBox_camera.TabIndex = 3;
+            this.textBox_camera.Text = "3";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "カメラ(y軸周り)";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(1, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 12);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "光源(xy,xy-z)";
+            // 
+            // textBox_0b
+            // 
+            this.textBox_0b.Location = new System.Drawing.Point(58, 197);
+            this.textBox_0b.Name = "textBox_0b";
+            this.textBox_0b.Size = new System.Drawing.Size(40, 19);
+            this.textBox_0b.TabIndex = 5;
+            this.textBox_0b.Text = "60";
+            // 
+            // textBox_1b
+            // 
+            this.textBox_1b.Location = new System.Drawing.Point(58, 222);
+            this.textBox_1b.Name = "textBox_1b";
+            this.textBox_1b.Size = new System.Drawing.Size(40, 19);
+            this.textBox_1b.TabIndex = 9;
+            this.textBox_1b.Text = "60";
+            // 
+            // textBox_2b
+            // 
+            this.textBox_2b.Location = new System.Drawing.Point(58, 247);
+            this.textBox_2b.Name = "textBox_2b";
+            this.textBox_2b.Size = new System.Drawing.Size(40, 19);
+            this.textBox_2b.TabIndex = 12;
+            this.textBox_2b.Text = "45";
+            // 
+            // textBox_3b
+            // 
+            this.textBox_3b.Location = new System.Drawing.Point(58, 272);
+            this.textBox_3b.Name = "textBox_3b";
+            this.textBox_3b.Size = new System.Drawing.Size(40, 19);
+            this.textBox_3b.TabIndex = 15;
+            this.textBox_3b.Text = "45";
+            // 
+            // textBox_3a
+            // 
+            this.textBox_3a.Location = new System.Drawing.Point(16, 272);
+            this.textBox_3a.Name = "textBox_3a";
+            this.textBox_3a.Size = new System.Drawing.Size(38, 19);
+            this.textBox_3a.TabIndex = 20;
+            this.textBox_3a.Text = "135";
+            // 
+            // textBox_2a
+            // 
+            this.textBox_2a.Location = new System.Drawing.Point(16, 247);
+            this.textBox_2a.Name = "textBox_2a";
+            this.textBox_2a.Size = new System.Drawing.Size(38, 19);
+            this.textBox_2a.TabIndex = 19;
+            this.textBox_2a.Text = "-135";
+            // 
+            // textBox_1a
+            // 
+            this.textBox_1a.Location = new System.Drawing.Point(16, 222);
+            this.textBox_1a.Name = "textBox_1a";
+            this.textBox_1a.Size = new System.Drawing.Size(38, 19);
+            this.textBox_1a.TabIndex = 18;
+            this.textBox_1a.Text = "45";
+            // 
+            // textBox_0a
+            // 
+            this.textBox_0a.Location = new System.Drawing.Point(16, 197);
+            this.textBox_0a.Name = "textBox_0a";
+            this.textBox_0a.Size = new System.Drawing.Size(38, 19);
+            this.textBox_0a.TabIndex = 17;
+            this.textBox_0a.Text = "-45";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(0, 200);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(11, 12);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(0, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(11, 12);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(0, 250);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(11, 12);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "2";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(0, 275);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(11, 12);
+            this.label6.TabIndex = 24;
+            this.label6.Text = "3";
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(749, 489);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_3a);
+            this.Controls.Add(this.textBox_2a);
+            this.Controls.Add(this.textBox_1a);
+            this.Controls.Add(this.textBox_0a);
+            this.Controls.Add(this.textBox_3b);
+            this.Controls.Add(this.textBox_2b);
+            this.Controls.Add(this.textBox_1b);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox_0b);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_camera);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button_開く);
+            this.Controls.Add(this.pictureBoxIpl1);
+            this.Name = "main";
+            this.Text = "main";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenCvSharp.UserInterface.PictureBoxIpl pictureBoxIpl1;
+        private System.Windows.Forms.Button button_開く;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton_3;
+        private System.Windows.Forms.RadioButton radioButton_2;
+        private System.Windows.Forms.RadioButton radioButton_1;
+        private System.Windows.Forms.RadioButton radioButton_0;
+        private System.Windows.Forms.TextBox textBox_camera;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_0b;
+        private System.Windows.Forms.TextBox textBox_1b;
+        private System.Windows.Forms.TextBox textBox_2b;
+        private System.Windows.Forms.TextBox textBox_3b;
+        private System.Windows.Forms.TextBox textBox_3a;
+        private System.Windows.Forms.TextBox textBox_2a;
+        private System.Windows.Forms.TextBox textBox_1a;
+        private System.Windows.Forms.TextBox textBox_0a;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Lambertian_photometric_stereo/main.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using OpenCvSharp;
+using MathNet;
+using MathNet.Numerics.LinearAlgebra.Double;
+
+namespace Lambertian_photometric_stereo
+{
+    public partial class main : Form
+    {
+        public static IplImage[] 入力画像 = new IplImage[4];
+        string[] ファイル名 = new string[4];
+        public static IplImage 出力画像;
+        public static Boolean is4Image = false;
+
+        DenseMatrix camera_v, light0_v, light1_v, light2_v, light3_v;
+
+        public main()
+        {
+            InitializeComponent();
+            ベクトル計算();
+        }
+        void ベクトル計算()
+        {
+            var camera_num = double.Parse(textBox_camera.Text);
+            var deg2rad = Math.PI / 180.0;
+            camera_v = DenseMatrix.OfArray(new double[,] { { Math.Sin(deg2rad * camera_num), 0, Math.Cos(deg2rad * camera_num) } });
+
+            light0_v = DenseMatrix.OfArray(new double[,] { { -Math.Cos(deg2rad*double.Parse(textBox_0b.Text)) * Math.Cos(deg2rad*double.Parse(textBox_0a.Text)),
+                                                             -Math.Cos(deg2rad*double.Parse(textBox_0b.Text)) * Math.Sin(deg2rad*double.Parse(textBox_0a.Text)), 
+                                                              Math.Sin(deg2rad*double.Parse(textBox_0b.Text)), } });
+            light1_v = DenseMatrix.OfArray(new double[,] { { -Math.Cos(deg2rad*double.Parse(textBox_1b.Text)) * Math.Cos(deg2rad*double.Parse(textBox_1a.Text)),
+                                                             -Math.Cos(deg2rad*double.Parse(textBox_1b.Text)) * Math.Sin(deg2rad*double.Parse(textBox_1a.Text)), 
+                                                              Math.Sin(deg2rad*double.Parse(textBox_1b.Text)), } });
+            light2_v = DenseMatrix.OfArray(new double[,] { { -Math.Cos(deg2rad*double.Parse(textBox_2b.Text)) * Math.Cos(deg2rad*double.Parse(textBox_2a.Text)),
+                                                             -Math.Cos(deg2rad*double.Parse(textBox_2b.Text)) * Math.Sin(deg2rad*double.Parse(textBox_2a.Text)), 
+                                                              Math.Sin(deg2rad*double.Parse(textBox_2b.Text)), } });
+            light3_v = DenseMatrix.OfArray(new double[,] { { -Math.Cos(deg2rad*double.Parse(textBox_3b.Text)) * Math.Cos(deg2rad*double.Parse(textBox_3a.Text)),
+                                                             -Math.Cos(deg2rad*double.Parse(textBox_3b.Text)) * Math.Sin(deg2rad*double.Parse(textBox_3a.Text)), 
+                                                              Math.Sin(deg2rad*double.Parse(textBox_3b.Text)), } });
+
+            Console.WriteLine(camera_v);
+            Console.WriteLine(light0_v);
+            Console.WriteLine(light1_v);
+            Console.WriteLine(light2_v);
+            Console.WriteLine(light3_v);
+ 
+        }
+        private void Click_開く(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (入力画像[i] != null) 入力画像[i].Dispose();
+                入力画像[i] = Cv.CreateImage(new CvSize(640, 480), BitDepth.U8, 1);
+            }
+            if (出力画像 != null)
+            {
+                出力画像.Dispose();
+                出力画像 = Cv.CreateImage(new CvSize(640, 480), BitDepth.U8, 1);
+            }
+            OpenFileDialog dialog = new OpenFileDialog()
+            {
+                Multiselect = true,  // 複数選択の可否
+                Filter =  // フィルタ
+                "画像ファイル|*.bmp;*.gif;*.jpg;*.png|全てのファイル|*.*",
+            };
+            //ダイアログを表示
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                //OKボタンがクリックされたとき
+                //選択されたファイル名をすべて表示する
+                foreach (var file in dialog.FileNames.Select((value, index) => new { value, index }))
+                {
+                    入力画像[file.index] = new IplImage(file.value, LoadMode.GrayScale);
+                    ファイル名[file.index]=dialog.SafeFileNames[file.index];
+                }
+                if (dialog.FileNames.Length == 4) is4Image = true;
+                else is4Image = false;
+                // ファイル名をタイトルバーに設定
+                this.Text = dialog.FileNames[0];
+
+                pictureBoxIpl1.ImageIpl = 入力画像[0];
+            }
+        }
+
+        private void Click_radio0(object sender, EventArgs e)//ラジオボタンは選択された時にのみクリックイベントが発生
+        {
+            pictureBoxIpl1.RefreshIplImage(入力画像[0]);
+            this.Text = ファイル名[0];
+        }
+
+        private void Click_radio1(object sender, EventArgs e)
+        {
+            pictureBoxIpl1.RefreshIplImage(入力画像[1]);
+            this.Text = ファイル名[1];
+        }
+
+        private void Click_radio2(object sender, EventArgs e)
+        {
+            pictureBoxIpl1.RefreshIplImage(入力画像[2]);
+            this.Text = ファイル名[2];
+        }
+
+        private void Click_radio3(object sender, EventArgs e)
+        {
+            pictureBoxIpl1.RefreshIplImage(入力画像[3]);
+            this.Text = ファイル名[3];
+        }
+        
+ 
+    }
+}
