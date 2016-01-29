ab496f57acb710c2417343a3bd68ebd9556f5a1e
c13proto <c13proto@aqua.ocn.ne.jp>
2016-01-29 20:40:26 +0900

    光源ベクトルの計算方法修正
    カメラの傾きを，xy平面のy軸周りの回転として光源のベクトルを修正

--- Lambertian_photometric_stereo/main.Designer.cs
+++ Lambertian_photometric_stereo/main.Designer.cs
@@ -50,6 +50,8 @@
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
+            this.button_テンプレ = new System.Windows.Forms.Button();
+            this.button_実行 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxIpl1)).BeginInit();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
@@ -65,7 +67,7 @@
             // 
             // button_開く
             // 
-            this.button_開く.Location = new System.Drawing.Point(3, 10);
+            this.button_開く.Location = new System.Drawing.Point(5, 12);
             this.button_開く.Name = "button_開く";
             this.button_開く.Size = new System.Drawing.Size(75, 23);
             this.button_開く.TabIndex = 1;
@@ -79,7 +81,7 @@
             this.panel1.Controls.Add(this.radioButton_2);
             this.panel1.Controls.Add(this.radioButton_1);
             this.panel1.Controls.Add(this.radioButton_0);
-            this.panel1.Location = new System.Drawing.Point(3, 39);
+            this.panel1.Location = new System.Drawing.Point(1, 71);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(95, 100);
             this.panel1.TabIndex = 2;
@@ -130,7 +132,7 @@
             // 
             // textBox_camera
             // 
-            this.textBox_camera.Location = new System.Drawing.Point(46, 157);
+            this.textBox_camera.Location = new System.Drawing.Point(44, 189);
             this.textBox_camera.Name = "textBox_camera";
             this.textBox_camera.Size = new System.Drawing.Size(44, 19);
             this.textBox_camera.TabIndex = 3;
@@ -139,7 +141,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(1, 142);
+            this.label1.Location = new System.Drawing.Point(-1, 174);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(76, 12);
             this.label1.TabIndex = 4;
@@ -148,7 +150,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(1, 182);
+            this.label2.Location = new System.Drawing.Point(-1, 214);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(74, 12);
             this.label2.TabIndex = 6;
@@ -156,7 +158,7 @@
             // 
             // textBox_0b
             // 
-            this.textBox_0b.Location = new System.Drawing.Point(58, 197);
+            this.textBox_0b.Location = new System.Drawing.Point(56, 229);
             this.textBox_0b.Name = "textBox_0b";
             this.textBox_0b.Size = new System.Drawing.Size(40, 19);
             this.textBox_0b.TabIndex = 5;
@@ -164,7 +166,7 @@
             // 
             // textBox_1b
             // 
-            this.textBox_1b.Location = new System.Drawing.Point(58, 222);
+            this.textBox_1b.Location = new System.Drawing.Point(56, 254);
             this.textBox_1b.Name = "textBox_1b";
             this.textBox_1b.Size = new System.Drawing.Size(40, 19);
             this.textBox_1b.TabIndex = 9;
@@ -172,7 +174,7 @@
             // 
             // textBox_2b
             // 
-            this.textBox_2b.Location = new System.Drawing.Point(58, 247);
+            this.textBox_2b.Location = new System.Drawing.Point(56, 279);
             this.textBox_2b.Name = "textBox_2b";
             this.textBox_2b.Size = new System.Drawing.Size(40, 19);
             this.textBox_2b.TabIndex = 12;
@@ -180,7 +182,7 @@
             // 
             // textBox_3b
             // 
-            this.textBox_3b.Location = new System.Drawing.Point(58, 272);
+            this.textBox_3b.Location = new System.Drawing.Point(56, 304);
             this.textBox_3b.Name = "textBox_3b";
             this.textBox_3b.Size = new System.Drawing.Size(40, 19);
             this.textBox_3b.TabIndex = 15;
@@ -188,7 +190,7 @@
             // 
             // textBox_3a
             // 
-            this.textBox_3a.Location = new System.Drawing.Point(16, 272);
+            this.textBox_3a.Location = new System.Drawing.Point(14, 304);
             this.textBox_3a.Name = "textBox_3a";
             this.textBox_3a.Size = new System.Drawing.Size(38, 19);
             this.textBox_3a.TabIndex = 20;
@@ -196,7 +198,7 @@
             // 
             // textBox_2a
             // 
-            this.textBox_2a.Location = new System.Drawing.Point(16, 247);
+            this.textBox_2a.Location = new System.Drawing.Point(14, 279);
             this.textBox_2a.Name = "textBox_2a";
             this.textBox_2a.Size = new System.Drawing.Size(38, 19);
             this.textBox_2a.TabIndex = 19;
@@ -204,7 +206,7 @@
             // 
             // textBox_1a
             // 
-            this.textBox_1a.Location = new System.Drawing.Point(16, 222);
+            this.textBox_1a.Location = new System.Drawing.Point(14, 254);
             this.textBox_1a.Name = "textBox_1a";
             this.textBox_1a.Size = new System.Drawing.Size(38, 19);
             this.textBox_1a.TabIndex = 18;
@@ -212,7 +214,7 @@
             // 
             // textBox_0a
             // 
-            this.textBox_0a.Location = new System.Drawing.Point(16, 197);
+            this.textBox_0a.Location = new System.Drawing.Point(14, 229);
             this.textBox_0a.Name = "textBox_0a";
             this.textBox_0a.Size = new System.Drawing.Size(38, 19);
             this.textBox_0a.TabIndex = 17;
@@ -221,7 +223,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(0, 200);
+            this.label3.Location = new System.Drawing.Point(-2, 232);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(11, 12);
             this.label3.TabIndex = 21;
@@ -230,7 +232,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(0, 225);
+            this.label4.Location = new System.Drawing.Point(-2, 257);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(11, 12);
             this.label4.TabIndex = 22;
@@ -239,7 +241,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(0, 250);
+            this.label5.Location = new System.Drawing.Point(-2, 282);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(11, 12);
             this.label5.TabIndex = 23;
@@ -248,11 +250,31 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(0, 275);
+            this.label6.Location = new System.Drawing.Point(-2, 307);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(11, 12);
             this.label6.TabIndex = 24;
             this.label6.Text = "3";
+            // 
+            // button_テンプレ
+            // 
+            this.button_テンプレ.Location = new System.Drawing.Point(5, 41);
+            this.button_テンプレ.Name = "button_テンプレ";
+            this.button_テンプレ.Size = new System.Drawing.Size(75, 23);
+            this.button_テンプレ.TabIndex = 25;
+            this.button_テンプレ.Text = "テンプレ";
+            this.button_テンプレ.UseVisualStyleBackColor = true;
+            this.button_テンプレ.Click += new System.EventHandler(this.Click_テンプレ);
+            // 
+            // button_実行
+            // 
+            this.button_実行.Location = new System.Drawing.Point(-2, 329);
+            this.button_実行.Name = "button_実行";
+            this.button_実行.Size = new System.Drawing.Size(75, 23);
+            this.button_実行.TabIndex = 26;
+            this.button_実行.Text = "実行";
+            this.button_実行.UseVisualStyleBackColor = true;
+            this.button_実行.Click += new System.EventHandler(this.Click_実行);
             // 
             // main
             // 
@@ -260,6 +282,8 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.ClientSize = new System.Drawing.Size(749, 489);
+            this.Controls.Add(this.button_実行);
+            this.Controls.Add(this.button_テンプレ);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
@@ -312,6 +336,8 @@
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button_テンプレ;
+        private System.Windows.Forms.Button button_実行;
     }
 }
 
--- Lambertian_photometric_stereo/main.cs
+++ Lambertian_photometric_stereo/main.cs
@@ -14,43 +14,106 @@ namespace Lambertian_photometric_stereo
 {
     public partial class main : Form
     {
-        public static IplImage[] 入力画像 = new IplImage[4];
+        IplImage[] 入力画像 = new IplImage[4];
         string[] ファイル名 = new string[4];
-        public static IplImage 出力画像;
-        public static Boolean is4Image = false;
+        IplImage テンプレ画像;
+        IplImage 出力画像;
 
-        DenseMatrix camera_v, light0_v, light1_v, light2_v, light3_v;
+        //double[] camera_v = new double[] {  Math.Sin(deg2rad * camera_num), 0, Math.Cos(deg2rad * camera_num)  };
+        double[,] light_v;//光源の向いてる方向のベクトル
+        double[] camera_light_v=new double[4];//カメラとライトのなす角度(rad)
+
+        double[,][] 表面角度;
+        int[,] 鏡面反射番号=new int[640,480];
 
         public main()
         {
             InitializeComponent();
-            ベクトル計算();
+            ベクトル格納();
         }
-        void ベクトル計算()
+        void ベクトル格納()
         {
-            var camera_num = double.Parse(textBox_camera.Text);
             var deg2rad = Math.PI / 180.0;
-            camera_v = DenseMatrix.OfArray(new double[,] { { Math.Sin(deg2rad * camera_num), 0, Math.Cos(deg2rad * camera_num) } });
 
-            light0_v = DenseMatrix.OfArray(new double[,] { { -Math.Cos(deg2rad*double.Parse(textBox_0b.Text)) * Math.Cos(deg2rad*double.Parse(textBox_0a.Text)),
-                                                             -Math.Cos(deg2rad*double.Parse(textBox_0b.Text)) * Math.Sin(deg2rad*double.Parse(textBox_0a.Text)), 
-                                                              Math.Sin(deg2rad*double.Parse(textBox_0b.Text)), } });
-            light1_v = DenseMatrix.OfArray(new double[,] { { -Math.Cos(deg2rad*double.Parse(textBox_1b.Text)) * Math.Cos(deg2rad*double.Parse(textBox_1a.Text)),
-                                                             -Math.Cos(deg2rad*double.Parse(textBox_1b.Text)) * Math.Sin(deg2rad*double.Parse(textBox_1a.Text)), 
-                                                              Math.Sin(deg2rad*double.Parse(textBox_1b.Text)), } });
-            light2_v = DenseMatrix.OfArray(new double[,] { { -Math.Cos(deg2rad*double.Parse(textBox_2b.Text)) * Math.Cos(deg2rad*double.Parse(textBox_2a.Text)),
-                                                             -Math.Cos(deg2rad*double.Parse(textBox_2b.Text)) * Math.Sin(deg2rad*double.Parse(textBox_2a.Text)), 
-                                                              Math.Sin(deg2rad*double.Parse(textBox_2b.Text)), } });
-            light3_v = DenseMatrix.OfArray(new double[,] { { -Math.Cos(deg2rad*double.Parse(textBox_3b.Text)) * Math.Cos(deg2rad*double.Parse(textBox_3a.Text)),
-                                                             -Math.Cos(deg2rad*double.Parse(textBox_3b.Text)) * Math.Sin(deg2rad*double.Parse(textBox_3a.Text)), 
-                                                              Math.Sin(deg2rad*double.Parse(textBox_3b.Text)), } });
+            light_v = new double[,] 
+            {   
+                {   -Math.Cos(deg2rad*double.Parse(textBox_0b.Text)) * Math.Cos(deg2rad*double.Parse(textBox_0a.Text)),
+                    -Math.Cos(deg2rad*double.Parse(textBox_0b.Text)) * Math.Sin(deg2rad*double.Parse(textBox_0a.Text)), 
+                     Math.Sin(deg2rad*double.Parse(textBox_0b.Text))
+                },
 
-            Console.WriteLine(camera_v);
-            Console.WriteLine(light0_v);
-            Console.WriteLine(light1_v);
-            Console.WriteLine(light2_v);
-            Console.WriteLine(light3_v);
+                {   -Math.Cos(deg2rad*double.Parse(textBox_1b.Text)) * Math.Cos(deg2rad*double.Parse(textBox_1a.Text)),
+                    -Math.Cos(deg2rad*double.Parse(textBox_1b.Text)) * Math.Sin(deg2rad*double.Parse(textBox_1a.Text)), 
+                     Math.Sin(deg2rad*double.Parse(textBox_1b.Text))
+                },
+
+                {   -Math.Cos(deg2rad*double.Parse(textBox_2b.Text)) * Math.Cos(deg2rad*double.Parse(textBox_2a.Text)),
+                    -Math.Cos(deg2rad*double.Parse(textBox_2b.Text)) * Math.Sin(deg2rad*double.Parse(textBox_2a.Text)), 
+                     Math.Sin(deg2rad*double.Parse(textBox_2b.Text))
+                },
+
+                {   -Math.Cos(deg2rad*double.Parse(textBox_3b.Text)) * Math.Cos(deg2rad*double.Parse(textBox_3a.Text)),
+                    -Math.Cos(deg2rad*double.Parse(textBox_3b.Text)) * Math.Sin(deg2rad*double.Parse(textBox_3a.Text)), 
+                     Math.Sin(deg2rad*double.Parse(textBox_3b.Text)) 
+                },
+            };
+            for (int i = 0; i < 4; i++) Console.WriteLine("{0},{1},{2}", light_v[i, 0], light_v[i, 1], light_v[i, 2]);
+            //カメラの回転角度を，光源ベクトルのy軸周りの逆回転とみなして調整
+            var camera_num = double.Parse(textBox_camera.Text);
+            double cos = Math.Cos(-deg2rad * camera_num),
+                    sin = Math.Sin(-deg2rad * camera_num);
+            for (int r = 0; r < 4; r++)
+            {
+                double x = light_v[r, 0],
+                        z = light_v[r, 2];
+                light_v[r, 0] = x * cos-z*sin;
+                light_v[r, 2] = x * sin + z * cos;
+            }
+
+            for (int i = 0; i < 4; i++) Console.WriteLine("{0},{1},{2}", light_v[i, 0], light_v[i, 1],light_v[i,2]);
+            //for (int r = 0; r <4; r++)
+            //{//カメラと光源のなす角度の計算
+            //    double 内積=0;
+            //    double norm_camera=0, norm_light=0;
+            //    for (int c = 0; c < 3; c++)
+            //    {
+            //        内積 += camera_v[c] * light_v[r, c];
+            //        norm_camera += camera_v[c] * camera_v[c];
+            //        norm_light += light_v[r, c] * light_v[r, c];
+            //    }
+            //    camera_light_v[r] = Math.Acos(内積 / (norm_camera * norm_light));
+            //    //Console.WriteLine(camera_light_v[r]*180.0/Math.PI);
+            //}
  
+        }
+        void 表面角度計算()
+        {
+            Console.WriteLine("表面角度計算開始");
+            for (int y = 0; y < 入力画像[0].Height; y++)
+            {
+                for (int x = 0; x < 入力画像[0].Width; x++)
+                {
+                    if (Cv.Get2D(テンプレ画像, y, x).Val0 != 255) 鏡面反射番号取得(x, y);
+                    //if(x%10==0&&y%10==0)Console.Write(鏡面反射番号[x,y]);
+                }
+                //if(y%10==0)Console.WriteLine("");
+            }
+            Console.WriteLine("表面角度計算終了");
+        }
+        void 鏡面反射番号取得(int x,int y)
+        {
+            double max_val = 0;
+            int max_num = 0;
+            for (int i = 0; i < 4; i++)
+            {
+                var val = Cv.Get2D(入力画像[i], y, x).Val0;
+                if (val > max_val)
+                {
+                    max_val = val;
+                    max_num = i;//一番輝度値の大きい画像の番号
+                }
+            }
+            鏡面反射番号[x, y] = max_num;
         }
         private void Click_開く(object sender, EventArgs e)
         {
@@ -81,15 +144,39 @@ namespace Lambertian_photometric_stereo
                     入力画像[file.index] = new IplImage(file.value, LoadMode.GrayScale);
                     ファイル名[file.index]=dialog.SafeFileNames[file.index];
                 }
-                if (dialog.FileNames.Length == 4) is4Image = true;
-                else is4Image = false;
                 // ファイル名をタイトルバーに設定
                 this.Text = dialog.FileNames[0];
 
                 pictureBoxIpl1.ImageIpl = 入力画像[0];
             }
         }
+        private void Click_テンプレ(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog()
+            {
+                Multiselect = false,  // 複数選択の可否
+                Filter =  // フィルタ
+                "画像ファイル|*.bmp;*.gif;*.jpg;*.png|全てのファイル|*.*",
+            };
+            //ダイアログを表示
+            DialogResult result = dialog.ShowDialog();
+            if (result == DialogResult.OK)
+            {
 
+                //OKボタンがクリックされたとき
+                //選択されたファイル名をすべて表示する
+                foreach (var file in dialog.FileNames.Select((value, index) => new { value, index }))
+                {
+                    テンプレ画像 = new IplImage(file.value, LoadMode.Color);
+                }
+                // ファイル名をタイトルバーに設定
+                this.Text = dialog.FileNames[0];
+            }
+        }
+        private void Click_実行(object sender, EventArgs e)
+        {
+            表面角度計算();
+        }
         private void Click_radio0(object sender, EventArgs e)//ラジオボタンは選択された時にのみクリックイベントが発生
         {
             pictureBoxIpl1.RefreshIplImage(入力画像[0]);
@@ -113,7 +200,7 @@ namespace Lambertian_photometric_stereo
             pictureBoxIpl1.RefreshIplImage(入力画像[3]);
             this.Text = ファイル名[3];
         }
-        
- 
+
+
     }
 }
